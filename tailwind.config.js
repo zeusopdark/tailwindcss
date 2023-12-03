d4c73cc7097f65580dfd2c41f021e8bcb454bc87
zeusopdark <ankitnishad400@gmail.com>
2023-12-04 02:25:40 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {
       screen: {
         'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
-        'tallscreen': { 'raw': '(min-aspect-ratio: 13/20)' },
+        'tallscreen': { 'raw': '(max-aspect-ratio: 13/20)' },
       },
       keyframes: {
         'open-menu': {
